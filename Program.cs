f7e4b2c9c8d7c1eb5122da6a1cecf7f015821413
BuildTools <unconfigured@null.spigotmc.org>
2024-03-06 13:54:11 -0300

    added json controller and moved global usings to their own file

--- Program.cs
+++ Program.cs
@@ -1,14 +1,21 @@
-global using  System.Data;
+using System.Text.Json.Serialization;
 using Jojo.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Logging.AddConsole();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IDataContext, DataContext>();
 builder.Services.AddSingleton<ICharacterData, CharacterData>();
+builder.Services.AddControllers().AddJsonOptions(x =>
+{
+    x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    x.JsonSerializerOptions.Converters.Add(new DateOnlyJsonConverter());
+    x.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+});
 
 
 var app = builder.Build();
