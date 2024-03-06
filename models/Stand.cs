6beafa2a128f79aa6110fb6a516ef2de12b55f13
BuildTools <unconfigured@null.spigotmc.org>
2024-03-06 13:46:16 -0300

    Changed property from character type to string

--- models/Stand.cs
+++ models/Stand.cs
@@ -2,10 +2,10 @@ namespace Jojo.Models;
 
 public class Stand
 {
-    public int Id { get; set; }
+    public int? Stand_id { get; set; }
     public required string Name { get; set; }
     public string? LocalizedName { get; set; }
-    public required Character User { get; set; }
+    public required string User { get; set; }
     public required string Type { get; set; }
     public bool Humanoid { get; set; }
     
