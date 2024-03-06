629c39846fe6dc35b4e187a6c32dc983fc0772b8
BuildTools <unconfigured@null.spigotmc.org>
2024-03-06 13:51:40 -0300

    created Appareances and Parts models

---
+++ models/Appareances.cs
@@ -0,0 +1,7 @@
+namespace Jojo.Models;
+public class Appareances
+{
+    public int Char_id { get; set; }
+    public int Part_id { get; set; }
+    public required Part Name { get; set; }
+}
---
+++ models/Parts.cs
@@ -0,0 +1,6 @@
+namespace Jojo.Models;
+
+public class Part
+{
+    public required string Name { get; set; }
+}
