376bda13fe050a2fd3ebd30f1c3c5a2e564113b4
BuildTools <unconfigured@null.spigotmc.org>
2024-03-06 13:58:09 -0300

    changed query and mapping

--- data/CharacterData.cs
+++ data/CharacterData.cs
@@ -1,6 +1,5 @@
 using Jojo.Models;
 using Dapper;
-using Npgsql;
 
 namespace Jojo.Data;
 
@@ -10,15 +9,29 @@ public class CharacterData(IDataContext db) : ICharacterData
 
     public async Task<IEnumerable<Character>> GetAll()
     {
-        string sqlquery = "select * from characters left join stands using(char_id);";
+        string sqlquery = @"SELECT * FROM characters
+                            left JOIN stands ON characters.char_id = stands.char_id
+                            left JOIN appareances ON characters.char_id = appareances.char_id
+                            left JOIN parts ON appareances.part_id = parts.part_id
+                            ";
         var db = _db.DbConnection();
-        return await db.QueryAsync<Character, Stand, Character>(sqlquery, map: (Character, Stand) =>{
+        var result = await db.QueryAsync<Character, Stand, Appareances, Part, Character>(sqlquery, map: (Character, Stand, Appareances, Part) =>
+        {
+            Character.FullName = Character.FullName.Trim();
             Character.Stand = Stand;
+            if(Stand != null)
+                Stand.User = Character.FullName;
+            Character.Appareances = [Part.Name];
             return Character;
-        }, splitOn: "stand_id");
+            
+        }, splitOn:"stand_id, char_id, part_id");
 
+        return result.GroupBy(x => x.Char_id).Select(y =>
+        {
+            var grouped = y.First();
+            grouped.Appareances = y.Select(x => x.Appareances.Single()).ToList();
+            return grouped;
+        });
 
     }
-    // public Task<IEnumerable<Character>> InsertCharacter(Character character) => _db.SaveData<Character>("get_all", character);
-
-}   
+}
