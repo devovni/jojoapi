071f8b1483d06fc4590824f6c5b9f8e40f75bdfe
BuildTools <unconfigured@null.spigotmc.org>
2024-04-11 15:24:04 -0300

    changed to nullable

--- models/Stand.cs
+++ models/Stand.cs
@@ -1,12 +1,16 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using Dapper;
+
 namespace Jojo.Models;
 
 public class Stand
 {
     public int? Stand_id { get; set; }
-    public required string Name { get; set; }
+    public  string StandName { get; set; } = "Unknown";
     public string? LocalizedName { get; set; }
-    public required string User { get; set; }
-    public required string Type { get; set; }
-    public bool Humanoid { get; set; }
+    public  string? User { get; set; } = "Unknown";
+    public string? StandType { get; set; } = "Unknown";
+
+    public bool Humanoid { get; set; } = false;
     
 }
