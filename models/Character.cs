3858cb4e339bdb7f112094433a2f35c8c5f69723
BuildTools <unconfigured@null.spigotmc.org>
2024-04-11 15:22:45 -0300

    Changed required properties to nullable

--- models/Character.cs
+++ models/Character.cs
@@ -5,27 +5,27 @@ namespace Jojo.Models;
 public class Character
 {
     public int Char_id { get; set; }
-    public required string FullName { get; set; }
+    public string? FullName { get; set; }
     [JsonConverter(typeof(DateOnlyJsonConverter))]
     public DateTime Birthdate { get; set; }
     [JsonConverter(typeof(JsonStringEnumConverter<Sex>))]
-    public  Sex Sex {get; set;}
+    public Sex Sex { get; set; }
     public int Height_in_cm { get; set; }
     public int Weight_in_kg { get; set; }
-    public required string Nationality { get; set; }
-    public required ICollection<string> Appareances { get; set; }
+    public string? Nationality { get; set; }
+    public ICollection<string>? Appareances { get; set; }
     public Stand? Stand { get; set; }
 }
 
-    public enum Sex 
-    {
-        [PgName("Unknown")]
-        Unknown,
-        [PgName("Male")]
-        Male,
-        [PgName("Female")]
-        Female,
-        [PgName("NotApplicable")]
-        NotApplicable,
+public enum Sex
+{
+    [PgName("Unknown")]
+    Unknown,
+    [PgName("Male")]
+    Male,
+    [PgName("Female")]
+    Female,
+    [PgName("NotApplicable")]
+    NotApplicable,
 
-    }
+}
